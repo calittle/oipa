be03d746d9ab9a26d1e117d28982c1fc57e36fe4
C.A. Little <calittle@users.noreply.github.com>
2022-03-22 10:52:49 -0400

    QA

--- 11.3.0/vagrant/scripts/install.sh
+++ 11.3.0/vagrant/scripts/install.sh
@@ -22,18 +22,18 @@ echo 'INSTALLER: Started up'
 # Normally we would do this in the OS (OEL7) but since that box is controlled by
 # another group we'll manually adjust it here.
 if [ -f "/swapfile" ]; then
-  echo 'INSTALLER: extra swap file exists.'
+	echo 'INSTALLER: extra swap file exists.'
 else 
-  echo 'INSTALLER: adjusting swap'
-  fallocate -l 4G /swapfile
-  chown root:root /swapfile
-  chmod 0600 /swapfile
-  mkswap /swapfile
-  swapon /swapfile
-  swapon -s
-  grep -i --color swap /proc/meminfo
-  echo "/swapfile none            swap    sw              0       0" >> /etc/fstab
-  echo 'INSTALLER: swap adjusted.'
+	echo 'INSTALLER: adjusting swap'
+	fallocate -l 4G /swapfile
+	chown root:root /swapfile
+	chmod 0600 /swapfile
+	mkswap /swapfile
+	swapon /swapfile
+	swapon -s
+	grep -i --color swap /proc/meminfo
+	echo "/swapfile none            swap    sw              0       0" >> /etc/fstab
+	echo 'INSTALLER: swap adjusted.'
 fi
 
 # get up to date
@@ -74,124 +74,127 @@ echo 'INSTALLER: Environment variables set'
 
 # Install Oracle
 if [ -f "/opt/oracle/dbinstalled.txt" ]; then
-  echo "INSTALLER: Database setup skipped."
+	echo "INSTALLER: Database setup skipped."
 
 else
 
-  unzip -n /vagrant/LINUX.X64_193000_db_home.zip -d $ORACLE_HOME/
-  cp /vagrant/ora-response/db_install.rsp.tmpl /vagrant/ora-response/db_install.rsp
-  sed -i -e "s|###ORACLE_BASE###|$ORACLE_BASE|g" /vagrant/ora-response/db_install.rsp
-  sed -i -e "s|###ORACLE_HOME###|$ORACLE_HOME|g" /vagrant/ora-response/db_install.rsp
-  sed -i -e "s|###ORACLE_EDITION###|$ORACLE_EDITION|g" /vagrant/ora-response/db_install.rsp
-  chown oracle:oinstall -R $ORACLE_BASE
+	if [ -f "/opt/oracle/db-step1.txt" ]; then
+		echo "INSTALLER: Database unpack skipped."
+	else
+		unzip -n /vagrant/LINUX.X64_193000_db_home.zip -d $ORACLE_HOME/
+		cp /vagrant/ora-response/db_install.rsp.tmpl /vagrant/ora-response/db_install.rsp
+		sed -i -e "s|###ORACLE_BASE###|$ORACLE_BASE|g" /vagrant/ora-response/db_install.rsp
+		sed -i -e "s|###ORACLE_HOME###|$ORACLE_HOME|g" /vagrant/ora-response/db_install.rsp
+		sed -i -e "s|###ORACLE_EDITION###|$ORACLE_EDITION|g" /vagrant/ora-response/db_install.rsp
+		chown oracle:oinstall -R $ORACLE_BASE
 
-  su -l oracle -c "yes | $ORACLE_HOME/runInstaller -silent -ignorePrereqFailure -waitforcompletion -responseFile /vagrant/ora-response/db_install.rsp"
-  $ORACLE_BASE/oraInventory/orainstRoot.sh
-  $ORACLE_HOME/root.sh
-  rm /vagrant/ora-response/db_install.rsp
+		su -l oracle -c "yes | $ORACLE_HOME/runInstaller -silent -ignorePrereqFailure -waitforcompletion -responseFile /vagrant/ora-response/db_install.rsp"
+		$ORACLE_BASE/oraInventory/orainstRoot.sh
+		$ORACLE_HOME/root.sh
+		rm /vagrant/ora-response/db_install.rsp
+		su -l oracle -c "echo 'delete this file to unpack and setup database'>>/opt/oracle/db-step1.txt"
+		echo 'INSTALLER: Oracle software installed'
+	fi
+	if [ -f "/opt/oracle/db-step2.txt" ]; then
+		echo "INSTALLER: Listener setup skipped."
+	else
+	
+		# create sqlnet.ora, listener.ora and tnsnames.ora
+		su -l oracle -c "mkdir -p $ORACLE_HOME/network/admin"
+		su -l oracle -c "echo 'NAME.DIRECTORY_PATH= (TNSNAMES, EZCONNECT, HOSTNAME)' > $ORACLE_HOME/network/admin/sqlnet.ora"
 
-  echo 'INSTALLER: Oracle software installed'
+		# Listener.ora
+		su -l oracle -c "echo 'LISTENER = (DESCRIPTION = (ADDRESS = (PROTOCOL = IPC)(KEY = EXTPROC1))(ADDRESS=(PROTOCOL=TCP)(HOST= 0.0.0.0)(PORT = $LISTENER_PORT)))' > $ORACLE_HOME/network/admin/listener.ora"
+		su -l oracle -c "echo 'DEDICATED_THROUGH_BROKER_LISTENER=ON' >> $ORACLE_HOME/network/admin/listener.ora"
+		su -l oracle -c "echo 'DIAG_ADR_ENABLED = off' >> $ORACLE_HOME/network/admin/listener.ora"
+		
 
-  # create sqlnet.ora, listener.ora and tnsnames.ora
-  su -l oracle -c "mkdir -p $ORACLE_HOME/network/admin"
-  su -l oracle -c "echo 'NAME.DIRECTORY_PATH= (TNSNAMES, EZCONNECT, HOSTNAME)' > $ORACLE_HOME/network/admin/sqlnet.ora"
+		su -l oracle -c "echo '$ORACLE_SID=localhost:$LISTENER_PORT/$ORACLE_SID' > $ORACLE_HOME/network/admin/tnsnames.ora"
+		su -l oracle -c "echo '$ORACLE_PDB=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=0.0.0.0)(PORT= $LISTENER_PORT))(CONNECT_DATA=(SERVER = DEDICATED)(SERVICE_NAME=$ORACLE_PDB)))' >> $ORACLE_HOME/network/admin/tnsnames.ora"
 
-  # Listener.ora
-  su -l oracle -c "echo 'LISTENER = 
-    (DESCRIPTION = 
-      (ADDRESS = (PROTOCOL = IPC)(KEY = EXTPROC1)) 
-      (ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = $LISTENER_PORT)) 
-  ) 
-  DEDICATED_THROUGH_BROKER_LISTENER=ON
-  DIAG_ADR_ENABLED = off
-  ' > $ORACLE_HOME/network/admin/listener.ora"
+		# Start LISTENER
+		su -l oracle -c "lsnrctl start"
+		su -l oracle -c "echo 'delete this file to setup listener database'>>/opt/oracle/db-step2.txt"
+		echo 'INSTALLER: Listener created'
+	fi
+	# Create database
+	if [ -f "/opt/oracle/db-step3.txt" ]; then
+		echo "INSTALLER: CDB/PDB setup skipped."
+	else    
+		# Auto generate ORACLE PWD if not passed on
+		export ORACLE_PWD=${ORACLE_PWD:-"`openssl rand -base64 8`1"}
+		echo "INSTALLER: ORACLE_PWD set to $ORACLE_PWD"
 
-  su -l oracle -c "echo '$ORACLE_SID=localhost:$LISTENER_PORT/$ORACLE_SID' > $ORACLE_HOME/network/admin/tnsnames.ora"
-  su -l oracle -c "echo '$ORACLE_PDB= 
-  (DESCRIPTION = 
-    (ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = $LISTENER_PORT))
-    (CONNECT_DATA =
-      (SERVER = DEDICATED)
-      (SERVICE_NAME = $ORACLE_PDB)
-    )
-  )' >> $ORACLE_HOME/network/admin/tnsnames.ora"
+		cp /vagrant/ora-response/dbca.rsp.tmpl /vagrant/ora-response/dbca.rsp
+		sed -i -e "s|###ORACLE_SID###|$ORACLE_SID|g" /vagrant/ora-response/dbca.rsp
+		sed -i -e "s|###ORACLE_PDB###|$ORACLE_PDB|g" /vagrant/ora-response/dbca.rsp
+		sed -i -e "s|###ORACLE_CHARACTERSET###|$ORACLE_CHARACTERSET|g" /vagrant/ora-response/dbca.rsp
+		sed -i -e "s|###ORACLE_PWD###|$ORACLE_PWD|g" /vagrant/ora-response/dbca.rsp
+		sed -i -e "s|###EM_EXPRESS_PORT###|$EM_EXPRESS_PORT|g" /vagrant/ora-response/dbca.rsp
 
-  # Start LISTENER
-  su -l oracle -c "lsnrctl start"
+		# Create DB
+		su -l oracle -c "dbca -silent -createDatabase -responseFile /vagrant/ora-response/dbca.rsp"
 
-  echo 'INSTALLER: Listener created'
+		# Post DB setup tasks
+		su -l oracle -c "sqlplus / as sysdba <<EOF
+			ALTER PLUGGABLE DATABASE $ORACLE_PDB SAVE STATE;
+			EXEC DBMS_XDB_CONFIG.SETGLOBALPORTENABLED (TRUE);
+			ALTER SYSTEM SET LOCAL_LISTENER = '(ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = $LISTENER_PORT))' SCOPE=BOTH;
+			ALTER SYSTEM REGISTER;
+			exit;
+			EOF"
+#    oipa-vagrant: -bash: line 6: warning: here-document at line 0 delimited by end-of-file (wanted `EOF')
+		rm /vagrant/ora-response/dbca.rsp
+		su -l oracle -c "echo 'delete this file to set CDB/PDB databases'>>/opt/oracle/db-step3.txt"
+		echo 'INSTALLER: Database created'
+	fi
+	if [ -f "/opt/oracle/db-step4.txt" ]; then
+		echo "INSTALLER: System registration skipped."
+	else   
+		sed -i -e "\$s|${ORACLE_SID}:${ORACLE_HOME}:N|${ORACLE_SID}:${ORACLE_HOME}:Y|" /etc/oratab
+		echo 'INSTALLER: Oratab configured'
 
-  # Create database
+		# configure systemd to start oracle instance on startup
+		sudo cp /vagrant/scripts/oracle-rdbms.service /etc/systemd/system/
+		sudo sed -i -e "s|###ORACLE_HOME###|$ORACLE_HOME|g" /etc/systemd/system/oracle-rdbms.service
+		sudo systemctl daemon-reload
+		sudo systemctl enable oracle-rdbms
+		sudo systemctl start oracle-rdbms
+		echo "INSTALLER: Created and enabled oracle-rdbms systemd's service"
 
-  # Auto generate ORACLE PWD if not passed on
-  export ORACLE_PWD=${ORACLE_PWD:-"`openssl rand -base64 8`1"}
-
-  cp /vagrant/ora-response/dbca.rsp.tmpl /vagrant/ora-response/dbca.rsp
-  sed -i -e "s|###ORACLE_SID###|$ORACLE_SID|g" /vagrant/ora-response/dbca.rsp
-  sed -i -e "s|###ORACLE_PDB###|$ORACLE_PDB|g" /vagrant/ora-response/dbca.rsp
-  sed -i -e "s|###ORACLE_CHARACTERSET###|$ORACLE_CHARACTERSET|g" /vagrant/ora-response/dbca.rsp
-  sed -i -e "s|###ORACLE_PWD###|$ORACLE_PWD|g" /vagrant/ora-response/dbca.rsp
-  sed -i -e "s|###EM_EXPRESS_PORT###|$EM_EXPRESS_PORT|g" /vagrant/ora-response/dbca.rsp
-
-  # Create DB
-  su -l oracle -c "dbca -silent -createDatabase -responseFile /vagrant/ora-response/dbca.rsp"
-
-  # Post DB setup tasks
-  su -l oracle -c "sqlplus / as sysdba <<EOF
-     ALTER PLUGGABLE DATABASE $ORACLE_PDB SAVE STATE;
-     EXEC DBMS_XDB_CONFIG.SETGLOBALPORTENABLED (TRUE);
-     ALTER SYSTEM SET LOCAL_LISTENER = '(ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = $LISTENER_PORT))' SCOPE=BOTH;
-     ALTER SYSTEM REGISTER;
-     exit;
-  EOF"
-
-  rm /vagrant/ora-response/dbca.rsp
-
-  echo 'INSTALLER: Database created'
-
-  sed -i -e "\$s|${ORACLE_SID}:${ORACLE_HOME}:N|${ORACLE_SID}:${ORACLE_HOME}:Y|" /etc/oratab
-  echo 'INSTALLER: Oratab configured'
-
-  # configure systemd to start oracle instance on startup
-  sudo cp /vagrant/scripts/oracle-rdbms.service /etc/systemd/system/
-  sudo sed -i -e "s|###ORACLE_HOME###|$ORACLE_HOME|g" /etc/systemd/system/oracle-rdbms.service
-  sudo systemctl daemon-reload
-  sudo systemctl enable oracle-rdbms
-  sudo systemctl start oracle-rdbms
-  echo "INSTALLER: Created and enabled oracle-rdbms systemd's service"
-
-  sudo cp /vagrant/scripts/setPassword.sh /home/oracle/
-  sudo chmod a+rx /home/oracle/setPassword.sh
-
-  echo "INSTALLER: setPassword.sh file setup";
-  echo "ORACLE PASSWORD FOR SYS, SYSTEM AND PDBADMIN: $ORACLE_PWD";
-  su -l oracle -c "echo 'delete this file to reinstall'>>/opt/oracle/dbinstalled.txt"
+		sudo cp /vagrant/scripts/setPassword.sh /home/oracle/
+		sudo chmod a+rx /home/oracle/setPassword.sh
+		echo "INSTALLER: setPassword.sh file setup";
+		su -l oracle -c "echo 'delete this file to register system services.'>>/opt/oracle/db-step4.txt"
+		echo "ORACLE PASSWORD FOR SYS, SYSTEM AND PDBADMIN: $ORACLE_PWD";
+	fi
+	su -l oracle -c "echo 'delete this file to reinstall'>>/opt/oracle/dbinstalled.txt"
 fi 
 
 # run user-defined post-setup scripts
 echo 'INSTALLER: Running user-defined post-setup scripts'
 
 for f in /vagrant/userscripts/*
-  do
-    case "${f,,}" in
-      *.sh)
-        echo "INSTALLER: Running $f"
-        . "$f"
-        echo "INSTALLER: Done running $f"
-        ;;
-      *.sql)
-        echo "INSTALLER: Running $f"
-        su -l oracle -c "echo 'exit' | sqlplus -s / as sysdba @\"$f\""
-        echo "INSTALLER: Done running $f"
-        ;;
-      /vagrant/userscripts/put_custom_scripts_here.txt)
-        :
-        ;;
-      *)
-        echo "INSTALLER: Ignoring $f"
-        ;;
-    esac
-  done
+	do
+		case "${f,,}" in
+			*.sh)
+				echo "INSTALLER: Running $f"
+				. "$f"
+				echo "INSTALLER: Done running $f"
+				;;
+			*.sql)
+				echo "INSTALLER: Running $f"
+				su -l oracle -c "echo 'exit' | sqlplus -s / as sysdba @\"$f\""
+				echo "INSTALLER: Done running $f"
+				;;
+			/vagrant/userscripts/put_custom_scripts_here.txt)
+				:
+				;;
+			*)
+				echo "INSTALLER: Ignoring $f"
+				;;
+		esac
+	done
 
 echo 'INSTALLER: Done running user-defined post-setup scripts'
 
--- 11.3.0/vagrant/userscripts/01-oipadb.sh
+++ 11.3.0/vagrant/userscripts/01-oipadb.sh
@@ -10,43 +10,60 @@
 # 
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 #
+if [ -f "/opt/oracle/oipadb.txt" ]; then
+	 echo "INSTALLER: OIPA DB setup skipped. Delete /opt/oracle/oipadb.txt to reprovision."
+else
+	echo 'INSTALLER: Started up 01-oipadb.sh'
+	# set up password for oipa/oipaivs
+	export ORACLE_PWD=${ORACLE_PWD:-"`openssl rand -base64 8`1"}
+	echo "ORACLE PASSWORD FOR $USER_OIPA and $USER_IVS : $ORACLE_PWD";
 
-echo 'INSTALLER: Started up 01-oipadb.sh'
+	# unzip oipa DB installer.
+	unzip -n /vagrant/V997069-01.zip -d /home/oracle
+	chown oracle:oinstall -R /home/oracle
 
-# set up password for oipa/oipaivs
-export ORACLE_PWD=${ORACLE_PWD:-"`openssl rand -base64 8`1"}
+	if [ -f "/opt/oracle/oipadb-step1.txt" ]; then
+	 echo "INSTALLER: OIPA user setup skipped; delete /opt/oracle/oipadb-step1.txt to reprovision."
+	else    
 
-# unzip oipa DB installer.
-unzip -n /vagrant/V997069-01.zip -d /home/oracle
-chown oracle:oinstall -R /home/oracle
+		# create users and import dir
+		su -l oracle -c "sqlplus / as sysdba <<EOF
+			alter session set container=orclpdb1;
+			create user $USER_OIPA identified by \"$ORACLE_PWD\";
+			grant connect, resource to $USER_OIPA;
+			grant unlimited tablespace to $USER_OIPA;   
+			create user $USER_IVS identified by \"$ORACLE_PWD\";
+			grant connect, resource to $USER_IVS;
+			grant unlimited tablespace to $USER_IVS;
+			create directory oipa_dir as '/home/oracle';
+			grant read, write on directory oipa_dir to system;
+			exit;
+			EOF"
+		su -l oracle -c "echo 'delete this file to recreate oipa users. Note you may need to drop everything that was created to redo this step!'>>/opt/oracle/oipadb-step1.txt"
+	fi
+	# TODO --> set up TDE 
 
-# create users and import dir
-su -l oracle -c "sqlplus / as sysdba <<EOF
-   alter session set container=orclpdb1;
-   create user $USER_OIPA identified by \"$ORACLE_PWD\";
-   grant connect, resource to $USER_OIPA;
-   grant unlimited tablespace to $USER_OIPA;   
-   create user $USER_IVS identified by \"$ORACLE_PWD\";
-   grant connect, resource to $USER_IVS;
-   grant unlimited tablespace to $USER_IVS;
-   create directory oipa_dir as '/home/oracle';
-   grant read, write on directory oipa_dir to system;
-   exit;
-EOF"
-echo "INSTALLER: $USER_OIPA created with password $ORACLE_PWD";
-echo "INSTALLER: $USER_IVS created with password $ORACLE_PWD";
+	# import the dump files
+	if [ -f "/opt/oracle/oipadb-step2.txt" ]; then
+		echo "INSTALLER: OIPA DB import skipped; delete /opt/oracle/oipadb-step2.txt to reprovision."
+	else    
+		# a hack to force the errors to return true so shell provisioner doesn't crap the bed.
+		su -l oracle -c "echo $ORACLE_PWD | impdp system@orclpdb1 directory=oipa_dir dumpfile=oipa_pas.dmp logfile=OIPA_PAS.log full=yes remap_schema=oipaqa:$USER_OIPA &> import_1.out | true"
+		echo "INSTALLER: oipa_pas.dmp imported. Check import_1.out for any notable errors as these will not be trapped!";
+		su -l oracle -c "echo 'delete this file to reimport oipa db. Note you may need to drop everything that was created to redo this step!'>>/opt/oracle/oipadb-step2.txt"
+	fi
+	if [ -f "/opt/oracle/oipadb-step3.txt" ]; then
+		echo "INSTALLER: OIPA IVS DB import skipped; delete /opt/oracle/oipadb-step3.txt to reprovision."
+	else    
+		su -l oracle -c "echo $ORACLE_PWD | impdp system@orclpdb1 directory=oipa_dir dumpfile=oipa_ivs.dmp logfile=OIPA_IVS.log full=yes remap_schema=oipa_ivs:$USER_IVS &> import_2.out | true"         
+		echo "INSTALLER: oipa_ivs.dmp imported. Check import_1.out for any notable errors as these will not be trapped!";
+		su -l oracle -c "echo 'delete this file to reimport oipa ivs db. Note you may need to drop everything that was created to redo this step!'>>/opt/oracle/oipadb-step3.txt"
+	fi
 
-# TODO --> set up TDE 
+	# TODO --> create read only user
 
-# import the dump files
-su -l oracle -c "echo $ORACLE_PWD | impdp system@orclpdb1 directory=oipa_dir dumpfile=oipa_pas.dmp logfile=OIPA_PAS.log full=yes remap_schema=oipaqa:$USER_OIPA"
- 
-echo "INSTALLER: oipa_pas.dmp imported.";
+	echo "ORACLE PASSWORD FOR $USER_OIPA and $USER_IVS : $ORACLE_PWD";
 
-su -l oracle -c "echo $ORACLE_PWD | impdp system@orclpdb1 directory=oipa_dir dumpfile=oipa_ivs.dmp logfile=OIPA_IVS.log full=yes remap_schema=oipa_ivs:$USER_IVS"
-
-echo "INSTALLER: oipa_ivs.dmp imported.";
-
-# TODO --> create read only user
-
-echo "INSTALLER: 01-oipadb.sh complete.";
+	su -l oracle -c "echo 'delete this file to reimport OIPA database. Note you may need to drop everything that was created to redo this step!'>>/opt/oracle/oipadb.txt"
+	echo "INSTALLER: 01-oipadb.sh complete.";
+fi
